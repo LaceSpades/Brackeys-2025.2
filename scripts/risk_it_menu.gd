317a21174a44174bbbd7bbc2d71acad740ca3f47
Brodie Robertson <brodierobertson54321@gmail.com>
2025-08-30 16:45:12 +0930

    Implement risk it display

--- scripts/risk_it_menu.gd
+++ scripts/risk_it_menu.gd
@@ -1,19 +1,37 @@
 extends Control
 
+@onready var display_timer: Timer = $DisplayTimer
+@onready var mode: Label = $Mode
+@onready var play_it_safe: Button = $PlayItSafe
+@onready var keep_going: Button = $KeepGoing
+@onready var risk_it: Button = $RiskIt
+@onready var retire: Button = $Retire
+
 func _on_retire_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	
+func disable_buttons() -> void:
+	play_it_safe.disabled = true
+	keep_going.disabled = true
+	risk_it.disabled = true
+	retire.disabled = true
+	display_timer.start()
 
 func _on_play_it_safe_pressed() -> void:
 	Globals.risk_it(-1)
-	return_to_game()
-
+	display_mode()
+	disable_buttons()
+	
 func _on_keep_going_pressed() -> void:
 	Globals.risk_it(0)
-	return_to_game()
 
 func _on_risk_it_pressed() -> void:
 	Globals.risk_it(1)
-	return_to_game()
+	display_mode()
+	disable_buttons()
 	
-func return_to_game() -> void:
+func display_mode() -> void:
+	mode.text = Globals.current_mode
+	
+func _on_display_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
